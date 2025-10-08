64dfc41a329a19aa87ddcc4b3ca7abbfc14e8bcc
Vivek Kushwaha <vivek@pseudoclan.com>
2025-10-08 11:16:31 +0530

    handled protected routes

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useLocalAuth } from './context/AuthContext';
+import useUser from './hooks/useUser';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -8,18 +9,19 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, allowedRoles }) => {
-  const { isAuthenticated, userRole } = useLocalAuth();
+  const { getCurrentUser } = useUser();
+  const {isPending, isSuccess, data: userData } = getCurrentUser;
   const location = useLocation();
 
-  if (!isAuthenticated) {
+  if (!isPending && (!isSuccess || userData.blocked)) {
     // Redirect to login page if not authenticated
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (allowedRoles && !allowedRoles.includes(userRole as string)) {
-    // Redirect to a forbidden page or homepage if role is not allowed
-    return <Navigate to="/forbidden" replace />; // You might want to create a Forbidden page
-  }
+  // if (allowedRoles && !allowedRoles.includes(userRole as string)) {
+  //   // Redirect to a forbidden page or homepage if role is not allowed
+  //   return <Navigate to="/forbidden" replace />; // You might want to create a Forbidden page
+  // }
 
   return <>{children}</>;
 };
